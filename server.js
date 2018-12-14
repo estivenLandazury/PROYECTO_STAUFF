39b06dc8f32749bd22fd0e21e6bdfacd8db13817
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2018-12-14 14:53:10 -0500

    Deploy angular

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
+// server.js
+//Install express server
 const express = require('express');
-const app = express();
+const http = require('http');
 const path = require('path');
-
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// ForceSSL middleware
-app.use(forceSSL());
+const app = express();
+// Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+})
+const port = process.env.PORT || 8080;
+app.set(port);
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running…'));
