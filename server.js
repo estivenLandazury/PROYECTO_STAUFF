6d49c532c2a4c45f06db4ffe7e4be45f7a550a36
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2018-12-14 15:20:14 -0500

    Deploy angular

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
+//Install express server    
 const express = require('express');
-const app = express();
-const path = require('path');
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// ForceSSL middleware
-app.use(forceSSL());
+const path = require('path');   
+
+const app = express();   
+
+// Serve only the static files form the dist directory    
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/my-proyecto-PDG/index.html'));
-});
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
